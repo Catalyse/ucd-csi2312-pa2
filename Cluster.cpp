136f7726b57b69362700dc4d20b4405549fb2e2f
Taylor may <spam@catalysetech.com>
2015-10-26 22:33:34 -0600

    Add, remove, cout, ==, complete

--- Cluster.cpp
+++ Cluster.cpp
@@ -25,33 +25,135 @@ namespace Clustering
     }
 
     // Set functions: They allow calling c1.add(c2.remove(p));
-    void add(const PointPtr & newPoint)
+    void Cluster::add(const PointPtr &newPoint)
     {
-    
+        LNodePtr node = new LNode();
+
+        if(points == nullptr)
+        {
+            points = node;
+            points->p = newPoint;
+            node->next = nullptr;
+        }
+        else
+        {
+            LNodePtr current = points;
+            PointPtr currentPoint = points->p;
+
+            for(int i = 0; i < size; i ++)
+            {
+                if(newPoint > currentPoint || newPoint == currentPoint)//We found where to put it
+                {
+                    LNodePtr temp = current; //Save point to move.
+
+                    current = node; //Assign blank node, add point, set next.
+                    current->p = newPoint;
+                    current->next = temp;
+                    break;
+                }
+                else if(current->next != nullptr) //move to the next val
+                {
+                    current = current->next;
+                    currentPoint = current->p;
+                }
+                else //List ended, add it to the end.
+                {
+                    current->next = node;
+                    current->next->p = newPoint;
+                    current->next->next = nullptr;
+                }
+            }
+        }
+        size++;
     }
 
-    const PointPtr &remove(const PointPtr & removePoint)
+    const PointPtr &Cluster::remove(const PointPtr & removePoint)
     {
-
+        if(points != nullptr)
+        {
+            LNodePtr current = points;
+            LNodePtr last;
+            while(current != nullptr)
+            {
+                if(current->p == removePoint)
+                    break;
+                else
+                {
+                    last = current;
+                    current = current->next;
+                }
+            }
+            if(current == nullptr)//If end of list is reached
+                std::cout << "Node Not Found in List!" << std::endl;//Alert user node is not in list.
+            else if(points == current)//remove head
+                points = points->next;
+            else
+                last->next = current->next;
+            delete current;
+            size--;
+        }
+        return removePoint;
     }
 
     // Overloaded operators
 
     // IO
-    std::ostream &operator<<(std::ostream &, const Cluster &); //nope
-    std::istream &operator>>(std::istream &, Cluster &);
+    std::ostream &operator<<(std::ostream &stream, const Cluster & cluster)
+    {
+        if (cluster.size != 0)
+        {
+            LNodePtr currentNode = cluster.points;//Current Node
+            PointPtr point = cluster.points->p;//Current Node point
 
+            std::cout << *point;
+
+            while(currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+                point = currentNode->p;
+                std::cout << *point;
+            }
+
+        }
+        else
+        {
+            std::cout << "Cluster is empty" << std::endl;
+        }
+
+        return stream;
+    }//nope
     // Set-preserving operators (do not duplicate points in the space)
     // - Friends
     bool operator==(const Cluster &lhs, const Cluster &rhs)
     {
+        LNodePtr currentR = rhs.points;
+        LNodePtr currentL = lhs.points;
 
+        if(lhs.size == rhs.size)
+        {
+            while(currentL != nullptr)
+            {
+                if(currentL->p != currentR->p)
+                    return false;
+                currentL = currentL->next;
+                currentR = currentR->next;
+            }
+            return true;
+        }
+        return false;
     }
 
     // - Members
     Cluster &Cluster::operator+=(const Cluster &rhs) // union
     {
-
+        if(rhs.size > 0)
+        {
+            LNodePtr current = rhs.points;
+            for(int i = 0; i < rhs.size; i++)
+            {
+                add()
+            }
+        }
     }
 
     Cluster &Cluster::operator-=(const Cluster &rhs) // (asymmetric) difference
