0c1eeaf72403ed29ff703d07c5b81aa7fb14f4ca
Taylor may <spam@catalysetech.com>
2015-12-16 23:22:44 -0700

    Wrote out header file for errors

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 namespace Clustering
 {
+    //---------------------------------------------DimsensionalityMismatchEx-----------------------------------------/
+    DimensionalityMismatchEx::DimensionalityMismatchEx(string name) {
+        this->resetError();
+        this->name = name;
+        this->errMessage = "Dimensionality Mismatch Error";
+    }
 
+    ostream &operator<<(ostream & os, DimensionalityMismatchEx & dimErr) {
+        os.width(4);
+        os << dimErr.getLine() << ": ";
+        os.width(65);
+        os.setf(ios::left, ios::adjustfield);
+        os << dimErr.getName() << "  ";
+        os.setf(ios::right, ios::adjustfield);
+        os << dimErr.getErr() << "\n";
+        os.flush();
+    }
+}
 }
--- Exceptions.h
+++ Exceptions.h
@@ -2,14 +2,60 @@
 #define CLUSTERING_EXCEPTIONS_H
 
 #include <string>
-#include <fstream>
-#include <limits>
+#include <iostream>
 
 #include "Cluster.h"
 
+using namespace std;
+
 namespace Clustering
 {
+    class DimensionalityMismatchEx
+    {
+    private:
+        string name = "Dimensionality Mismatch Error: ";
+        string message;
+    public:
+        // Constructors
+        DimensionalityMismatchEx(string);
 
+        // Getters
+        string getName() {return this->name;}
+        string getErr() {return this->message;}
+
+        friend std::ostream &operator<<(ostream &, DimensionalityMismatchEx &);
+
+    };
+
+    class OutOfBoundsEx
+    {
+
+    private:
+        string name = "Out Of Bounds Error: ";
+        string message;
+    public:
+        OutOfBoundsEx(string);
+
+        string getName() {return this->name;};
+        string getError() {return this->message;};
+
+        friend std::ostream &operator<<(ostream &, OutOfBoundsEx &);
+    };
+
+    class RemoveFromEmptyEx
+    {
+    private:
+        string name = "Remove From Empty Error: ";
+        string message;
+    public:
+        RemoveFromEmptyEx(string);
+
+        string getName() {return this->name;};
+        string getErr() {return this->message;};
+
+
+        friend std::ostream &operator<<(ostream &, RemoveFromEmptyEx &);
+    };
 }
 
 
